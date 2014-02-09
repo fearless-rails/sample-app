eae9e7e74fcd66af6653dda210bb664c1d1c943f
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-02-09 16:05:04 -0600

    Add restart command to Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute "cd #{current_path} && kill -USR2 `cat tmp/pids/unicorn.pid`"
     end
   end
 
